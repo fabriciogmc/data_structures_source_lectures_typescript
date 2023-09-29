bc4acc3681fe38e00be0e9249526323c927a1134
Fabrício Galende M. de Carvalho <fabriciogmc@gmail.com>
2023-09-29 13:49:20 -0300

    Binary and sequential search examples added

---
+++ busca/search.ts
@@ -0,0 +1,78 @@
+/* A basic sequential search algorithm 
+** implementation in TS
+**
+** Author: Fabrício Galende M. de Carvalho, DSc
+*/
+
+class Search<T>{
+    sequential(e:T, v:T[]):number{
+        let pos: number = -1;
+        for(let i:number =0; i< v.length; ++i){
+            if(e == v[i])
+                return i;
+        }
+        return pos;
+    }
+
+    sequential_w(e:T, v:T[]):number{
+        let pos:number = 0;
+        while(pos <v.length && v[pos]!= e)
+            ++pos;
+        if(pos == v.length)
+            return -1;
+        else 
+            return pos;        
+    }
+
+    /* sequential with sentinel */
+    sequential_ws(e:T, v:T[]):number{
+        let pos:number = 0;
+        v.push(e);
+        while(v[pos]!= e)
+            ++pos;
+        if(pos == (v.length-1)){
+            v.pop();
+            return -1;
+        } else{
+            v.pop();
+            return pos;
+        }                     
+    }
+        
+    binary(e:T, v:T[]):number{
+        let start = 0;
+        let end = v.length;
+        let middle = Math.floor((start + end)/2);
+        while( start <= end ){
+            if (v[middle] == e){
+                return middle
+            } else if(v[middle]>e){
+                        end = middle-1;
+                   } else {
+                        start = middle+1;
+                   }
+            middle = Math.floor((start+end)/2);
+        }
+        return -1;
+    }
+
+    binary_r(e:T, v:T[], start:number, end:number):number{ 
+        let middle = Math.floor((start+end)/2);   
+        if (start > end){    
+            return -1;
+        } else if (v[middle] >e ){
+                return this.binary_r(e, v, start, middle-1);
+        } else if (v[middle] <e ){                           
+                return this.binary_r(e,v, middle+1, end);
+        } else {
+                return middle;
+        }
+    }
+
+
+
+}
+
+export{
+    Search
+}
---
+++ busca/search_view.ts
@@ -0,0 +1,36 @@
+/* Sequential search execution examples
+**
+** Author: Fabricio Galende M. de Carvalho, DSc.
+*/
+
+import {Search} from './search'
+
+let s_string = new Search<string>();
+let s_number = new Search<number>();
+
+let v1: number[] = [2, 3, -3, 10, 5];
+let v2: string[] = ['joão', 'fabricio',
+                   'thales', 'fabíola'];
+
+                   
+console.log(s_string.sequential('fabricio',v2));
+console.log(s_string.sequential('eduardo',v2));
+
+console.log("Unordered numerical vector: ", v1)
+console.log("Sequential sort (found): ", s_number.sequential(10,v1));
+console.log("Sequential sort (not found): ", s_number.sequential(100,v1));
+console.log("Sequential sort while (not found): ", s_number.sequential_w(100,v1));
+console.log("Sequential sort while sentinel (not found): ", s_number.sequential_ws(100,v1));
+console.log("Unordered numerical vector (after sentinel): ", v1);
+
+/* Vector used to test binary search. It must
+** be an ordered vector*/
+let v_ord = [2, 4, 6, 8, 10, 12, 14, 16, 18];
+console.log("Binary search (found):", s_number.binary(16,v_ord))
+console.log("Binary search recursive (found):", s_number.binary_r(16,v_ord,0, v_ord.length-1));
+
+
+
+
+
+
